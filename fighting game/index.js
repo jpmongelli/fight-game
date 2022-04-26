de6788a3d9a02981e50851fb5521277d925b2c83
jpmongelli <61852102+jpmongelli@users.noreply.github.com>
2022-04-26 09:14:34 -0400

    Add files via upload

--- fighting game/index.js
+++ fighting game/index.js
@@ -1,4 +1,4 @@
-const canvas=document.querySelector(`canvas`);/*declara constante canvas para determinar uma area de renderizaçao de graficos, querySelector seleciona  aarea do site onde vai ser alterado*/
+const canvas=document.querySelector(`canvas`);/*declara constante canvas para determinar uma area de renderizaçao de graficos, querySelector seleciona  area do site onde vai ser alterado*/
 
 const c = canvas.getContext('2d');/*cria uma constante onde define no canvas o mode de renderizaçao grafica para 2d, podendo ser alterada para 3d*/
 
@@ -6,32 +6,203 @@ canvas.width = 1024/*define a largura da area de renderizaçao */
 canvas.height = 576/*define a altura da area de renderizaçao*/
 
 c.fillRect(0,0 , canvas.width, canvas.height)/*desenha um retangulo na tela*/
+const gravity = 0.7
 
 
-class Sprite {/* cria uma clase para usar orientaçao a objetos*/
-    constructor(position){/*cria um construtor, q recebe uma posiçao do objeto na tela*/
-       this.position = position
-    }
-    desenho(){
-        c.fillStyle='red'
-        c.fillRect(this.position.x,this.position.y,50,150)
-    }
-}
-const player = new Sprite({/* cria um objeto player */
-    x:0,
-    y:0
+
+const background = new Sprite({
+    position:{
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/background.png'
 })
-player.desenho()
 
-const inimigo = new Sprite({/*cria um objeto inimigo */
-    x:400,
-    y:100
+const shop = new Sprite({
+    position:{
+        x: 625,
+        y: 133
+    },
+    imageSrc: './img/shop.png',
+    scale:2.70,
+    framesMax:6
 })
-inimigo.desenho()
+
+const player = new Fighter({
+    position:{/* cria um objeto player */
+        x:0,
+        y:0
+    },
+    velocity:{
+        x: 0,
+        y: 0
+    },
+    offset:{
+        x:0,
+        y:0
+    },
+    imageSrc: './img/samuraiMack/idle.png',
+    scale:2.5,
+    framesMax:8,
+    offset:{
+        x:215,
+        y:154
+    }
+})
+
+
+const inimigo = new Fighter({/*cria um objeto inimigo */
+    position:{
+        x:400,
+        y:100
+    },
+    velocity:{
+        x:0,
+        y:0
+    },
+    color: 'blue',
+    offset:{
+        x:-50,
+        y:0
+    }
+})
+
 
 console.log(player)/* */
-
-
-function animacao(){
-    window.requestAnimationFrame
+const keys = {
+    a:{
+        pressed: false
+    },
+    d:{
+        pressed: false
+    },
+    w:{
+        pressed: false
+    },
+    ArrowLeft:{
+        pressed: false
+    },
+    ArrowRight:{
+        pressed:false
+    }
 }
+
+
+decreaseTimer()
+
+function animation(){/*cria uma funçao de animaçao em um loop infinito a te ser mandado parar*/
+    window.requestAnimationFrame(animation)
+    c.fillStyle='black'
+    c.fillRect(0,0,canvas.width,canvas.height)
+    
+    background.update()
+    shop.update()
+    player.update()
+    //inimigo.update()
+
+    player.velocity.x = 0
+    inimigo.velocity.x = 0
+    //player moviment
+    if(keys.a.pressed && player.lastKey === 'a'){
+        player.velocity.x = -5
+    }
+    else if(keys.d.pressed && player.lastKey === 'd'){
+        player.velocity.x = 5
+    }
+    // enemy moviment
+    if(keys.ArrowLeft.pressed && inimigo.lastKey === 'ArrowLeft'){
+        inimigo.velocity.x = -5
+    }
+    else if(keys.ArrowRight.pressed && inimigo.lastKey === 'ArrowRight'){
+        inimigo.velocity.x = 5
+    }
+    //detec for colision
+    if(rectangularColision({
+        rectangle1: player,
+        rectangle2: inimigo
+    }) &&
+        player.isAttaking
+    )   {
+        player.isAttaking = false
+        inimigo.health -= 20
+        document.querySelector('#enemyHealth').style.width = inimigo.health + '%'
+    }
+    if(rectangularColision({
+        rectangle1: inimigo,
+        rectangle2: player
+    }) &&
+        inimigo.isAttaking
+    )   {
+        inimigo.isAttaking = false
+        player.health -= 20
+        document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+    //end game based on health
+    if(inimigo.health <= 0 || player.health <= 0){
+        determineWinner({player, inimigo,timerId})
+    }
+}
+animation()
+/*window.addEventListener('keydown',()=>{}) cria um leitor de evento q le caso uma tecla seja precionada */
+/* movimentaçao player*/
+window.addEventListener('keydown',(event)=>{   
+    switch(event.key){
+        case 'd':
+            keys.d.pressed = true
+            player.lastKey = 'd'
+            break
+        case 'a':
+            keys.a.pressed = true 
+            player.lastKey ='a'
+            break
+        case 'w':
+            player.velocity.y =-20
+            break
+        case ' ':
+            player.attack()
+            break
+        /* movimentaçao inimigo*/
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            inimigo.lastKey = 'ArrowRight'
+        break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true 
+            inimigo.lastKey ='ArrowLeft'
+        break
+        case 'ArrowUp':
+            inimigo.velocity.y =-20
+        break
+        case 'ArrowDown':
+            inimigo.isAttaking=true
+             break
+    }
+})
+window.addEventListener('keyup',(event)=>{
+    switch(event.key){
+        case 'd':
+            keys.d.pressed = false
+        break
+        case 'a':
+            keys.a.pressed = false
+        break
+        case 'w':
+            keys.w.pressed = false
+        break
+    }
+    /* movimentaçao inimigo*/
+    switch(event.key){
+        case 'ArrowRight':
+        keys.ArrowRight.pressed = false
+            inimigo.lastKey = 'ArrowRight'
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+            inimigo.lastKey ='ArrowLeft'
+        break
+    }
+})
+
+
+
+
